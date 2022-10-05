a4b5d1207b3db4864b06cc075d95aa54e24c76a8
Sanskar Omar <heysanskar0.0@gmail.com>
2022-10-05 18:05:10 +0530

    Disabled bypassing Stage 1 on wrong input key

--- js/check_key.js
+++ js/check_key.js
@@ -1,4 +1,5 @@
 $("#form").submit(function (e) {
+	console.log("submit");
 	let userAnswer = $("#answer").val();
 	if (userAnswer == "") {
 		alert("Enter a valid Cipher!");
@@ -8,22 +9,34 @@ $("#form").submit(function (e) {
 		e.preventDefault();
 	}
 	else {
-		$.ajax({
-			url: "https://api.hashify.net/hash/md5/hex?value=" + userAnswer,
-			type: "GET",
-			dataType: "json",
-			success: function (data) {
-				if (data["Digest"] != "a542e9b744bedcfd874129ab0f98c4ff") {
-					// wrong answer
-					$("#answer").val("");
-					$("#answer").attr("placeholder", "Wrong answer");
-					$("#answer").focus();
-					alert("Inspection is the key");
-					e.preventDefault();
-				} else {
-					// correct answer
-					alert("Go0d job :thumbsup: 3asy enough!\nPu1l <up arrow> your sOcK8 4 de Next 1");
-				}
+		console.log("userAnswer: " + userAnswer + "going for api hit");
+		let hash = getPasswordHash(userAnswer);
+		hash.done(function (data) {
+			console.log("hash: " + data.Digest);
+			if (data.Digest != "a542e9b744bedcfd874129ab0f98c4ff") {
+				// wrong answer
+				$("#answer").val("");
+				$("#answer").attr("placeholder", "Wrong answer");
+				$("#answer").focus();
+				alert("Inspection is the key");
+				e.preventDefault();
+			} else {
+				// correct answer
+				alert("Go0d job :thumbsup: 3asy enough!\nPu1l <up arrow> your sOcK8 4 de Next 1");
+				console.log("Go0d job :thumbsup: 3asy enough!\nPu1l <up arrow> your sOcK8 4 de Next 1");
 			}
 		});
-	}});
+		console.log("else end");
+		e.preventDefault();
+	}
+	console.log("submit end");
+});
+
+function getPasswordHash(userAnswer) {
+	console.log("processing request");
+	return $.ajax({
+		url: "https://api.hashify.net/hash/md5/hex?value=" + userAnswer,
+		type: "GET",
+		dataType: "json",
+	});
+}
